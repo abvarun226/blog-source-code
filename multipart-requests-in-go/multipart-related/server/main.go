99ab8b04cfbfb8052b1f22f312b9c1d4af8f3f31
varun <varun@imgur.com>
2019-06-12 11:49:22 +0530

    Update readme

---
+++ multipart-requests-in-go/multipart-related/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		contentType, params, parseErr := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		if parseErr != nil || !strings.HasPrefix(contentType, "multipart/") {
+			http.Error(w, "expecting a multipart message", http.StatusBadRequest)
+			return
+		}
+
+		multipartReader := multipart.NewReader(r.Body, params["boundary"])
+		defer r.Body.Close()
+
+		for {
+			part, err := multipartReader.NextPart()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				http.Error(w, "unexpected error when retrieving a part of the message", http.StatusInternalServerError)
+				return
+			}
+			defer part.Close()
+		
+			fileBytes, err := ioutil.ReadAll(part)
+			if err != nil {
+				http.Error(w, "failed to read content of the part", http.StatusInternalServerError)
+				return
+			}
+
+			switch part.Header.Get("Content-ID") {
+			case "metadata":
+				log.Print(string(fileBytes))
+		
+			case "media":
+				log.Printf("filesize = %d", len(fileBytes))
+				f, _ := os.Create(part.Header.Get("Content-Filename"))
+				f.Write(fileBytes)
+				f.Close()
+			}
+		}
+	})
+
+	http.ListenAndServe(":8080", mux)
+}
